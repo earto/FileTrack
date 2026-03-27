6e4aa3ecfa0ffa847bdf004b26604e8526672c04
TheScubadiver <154598966+TheScubadiver@users.noreply.github.com>
2026-03-27 19:33:02 +0100

    Update __init__.py

--- custom_components/filetrack/__init__.py
+++ custom_components/filetrack/__init__.py
@@ -1,1 +1,15 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN
 
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Stel de integratie in via de GUI."""
+    hass.data.setdefault(DOMAIN, {})
+    
+    # Stuur de configuratie door naar de sensor
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Verwijder de integratie via de GUI."""
+    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
