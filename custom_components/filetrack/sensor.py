2fe476e398e84e36ede9c86abad14db6de7cea4f
TheScubadiver <154598966+TheScubadiver@users.noreply.github.com>
2026-03-27 19:33:15 +0100

    Update sensor.py

--- custom_components/filetrack/sensor.py
+++ custom_components/filetrack/sensor.py
@@ -1,1 +1,71 @@
+import glob
+import os
+import logging
+from datetime import timedelta
 
+from homeassistant.components.sensor import SensorEntity
+from .const import DOMAIN, CONF_FOLDER_PATHS, CONF_FILTER, CONF_SORT, CONF_RECURSIVE
+
+_LOGGER = logging.getLogger(__name__)
+SCAN_INTERVAL = timedelta(minutes=1)
+
+def get_files_list(folder_path, filter_term, sort, recursive):
+    query = os.path.join(folder_path, filter_term)
+    files = glob.glob(query, recursive=recursive)
+    if sort == "name":
+        return sorted(files)
+    elif sort == "size":
+        return sorted(files, key=os.path.getsize)
+    else: # date
+        return sorted(files, key=os.path.getmtime, reverse=True)
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Setup sensor platform via de Config Entry (GUI)."""
+    config = entry.data
+    
+    sensor = FileTrackSensor(
+        config[CONF_FOLDER_PATHS],
+        config["name"],
+        config[CONF_FILTER],
+        config[CONF_SORT],
+        config[CONF_RECURSIVE],
+        entry.entry_id
+    )
+    async_add_entities([sensor], True)
+
+class FileTrackSensor(SensorEntity):
+    """De sensor definitie."""
+    _attr_icon = "mdi:folder"
+    _attr_native_unit_of_measurement = "MB"
+
+    def __init__(self, folder_path, name, filter_term, sort, recursive, entry_id):
+        self._attr_name = name
+        self._attr_unique_id = f"filetrack_{entry_id}"
+        self._folder_path = os.path.join(folder_path, "")
+        self._filter_term = filter_term
+        self._sort = sort
+        self._recursive = recursive
+        self._state = 0
+        self._attributes = {}
+
+    def update(self):
+        """Haal de nieuwe data op."""
+        files_list = get_files_list(self._folder_path, self._filter_term, self._sort, self._recursive)
+        total_size = sum(os.path.getsize(f) for f in files_list if os.path.isfile(f))
+        
+        self._state = round(total_size / 1e6, 2)
+        self._attributes = {
+            "path": self._folder_path,
+            "filter": self._filter_term,
+            "number_of_files": len(files_list),
+            "fileList": files_list,
+            "sort": self._sort,
+        }
+
+    @property
+    def native_value(self):
+        return self._state
+
+    @property
+    def extra_state_attributes(self):
+        return self._attributes
